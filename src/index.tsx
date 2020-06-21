8949910b2c0c1be6340b797e33c3544d02b4ef4f
sekiya9311 <sekiya9311@gmail.com>
2020-06-21 21:10:12 +0900

    ゲーム勝者の判定
    > https://ja.reactjs.org/tutorial/tutorial.html#declaring-a-winner

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,28 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const calculateWinner = (
+  squares: ReadonlyArray<string | null>
+): string | null => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 interface SquareProps {
   value: string | null;
   onClick: () => void;
@@ -21,6 +43,9 @@ const Board: React.FC = () => {
 
   const handleClick = (i: number) => {
     const newSquares = squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
     newSquares[i] = currentPlayer;
     setSquares(newSquares);
     setXIsNext((x) => !x);
@@ -29,7 +54,8 @@ const Board: React.FC = () => {
     <Square value={squares[i]} onClick={() => handleClick(i)} />
   );
 
-  const status = `Next player: ${currentPlayer}`;
+  const winner = calculateWinner(squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${currentPlayer}`;
 
   return (
     <div>
